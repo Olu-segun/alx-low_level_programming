7b1b39d345b02b9b7b1475780beacde90e5b0246
olukayode olusegun <olukayodeoluseguno@gmail.com>
2022-10-14 01:18:37 -0700

    Printing size of character

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-  printf("size of char : %zu bytes\n", sizeof(char));
-  printf("size of int : %zu bytes\n", sizeof(int));
-  printf("size of long int : %zu bytes\n", sizeof(long int))
+  printf("size of a char : %zu bytes\n", sizeof(char));
+  printf("size of an int : %zu bytes\n", sizeof(int));
+  printf("size of a long int : %zu bytes\n", sizeof(long int))
   printf("size of a long long int :%zu bytes\n", sizeof(long long int));
-  printf("size of flaot : %zu bytes\n", sizeof(float));
+  printf("size of a flaot : %zu bytes\n", sizeof(float));
 
   return (0);
 }
