84ad0c770d48baea0707a112c7fd1786827c2dca
olukayode olusegun <olukayodeoluseguno@gmail.com>
2022-10-15 04:05:12 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Print alphabet using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp = 'a';
+
+	while (apl <= 'z')
+	{
+		putchar(apl);
+		++apl;
+	}
+	putchar('\n');
+	return (0);
+}
