ba47959430daf17801206459322f2566b61aa97c
olukayode olusegun <olukayodeoluseguno@gmail.com>
2022-10-15 05:00:04 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *  main - Print alphabet in lowercase, and then in uppercase.
+ *
+ *  Return 0 (Success)
+ */
+int main(void)
+{
+	char alp = 'a';
+	char ALP = 'A';
+
+	while (alp <= 'z')
+	{
+		putchar(alp);
+		++alp;
+	}
+
+	while (ALP <= 'Z')
+	{
+		putchar(ALP);
+		++ALP;
+	}
+	putchar('\n');
+	return (0);
+}
