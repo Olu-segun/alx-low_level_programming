0f2fbb89421e54cd19e340c2b50d7f006df0f010
olukayode olusegun <olukayodeoluseguno@gmail.com>
2022-10-16 06:33:05 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase except letter 'e' and 'q'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp;
+
+	for alp(alp = 'a'; apl = <= 'z'; alp++)
+	{
+		if (alp != 'e' && alp != 'z')
+			putchar(alp);
+	}
+	putchar('\n');
+	return (0);
+}
