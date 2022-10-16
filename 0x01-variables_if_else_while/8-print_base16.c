70bc05dc51431a412d09b2eec9af93a2d5f57a21
olukayode olusegun <olukayodeoluseguno@gmail.com>
2022-10-16 09:14:59 -0700

    a program that prints all the numbers of base 16 in lowercase, followed by a new line.

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -15,7 +15,7 @@ int main(void)
 	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
 
-	for (alp = 'a'; alp < 'f'; alp++)
+	for (alp = 'a'; alp <= 'f'; alp++)
 		putchar(alp);
 
 	putchar('\n');
