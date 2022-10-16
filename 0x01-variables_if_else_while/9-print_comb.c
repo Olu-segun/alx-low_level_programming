52508e7b7c19029bf0bbaaadf81c7f04eeb3504a
olukayode olusegun <olukayodeoluseguno@gmail.com>
2022-10-16 09:45:58 -0700

    A program that prints all possible combinations of single-digit numbers.

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,32 @@
+ #include <stio.h>
+/**
+ * main - Entry point.
+ *
+ * Description: Prints all possible combination of single-digit number.
+ *
+ * Return: Always  0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+
+		if (num == '9')
+		{
+			break;
+		}
+		else
+		{
+			putchar(',');
+			putchat(' ');
+		}
+	}
+		putchar('\n');
+		return (0);
+}
+
+
