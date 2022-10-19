4db1c8e4c55f966b553db6c9eff7ab5a2a4fdbbd
olukayode olusegun <olukayodeoluseguno@gmail.com>
2022-10-19 08:28:52 -0700

    A function that prints the last digit of a number.

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_to_98 - Entry point.
+ * Desc: A function that prints all natural numbers from n to 98
+ * @n: The number to begin counting at.
+ * Return: Always 0 (Success)
+ */
+void print_to_98(int n)
+{
+	if ( n 
