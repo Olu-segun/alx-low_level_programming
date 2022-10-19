7bf6b58f5f1ba90425d173c305c4f682f02594ae
olukayode olusegun <olukayodeoluseguno@gmail.com>
2022-10-19 02:39:58 -0700

    A function that checks for alphabetic character.

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,13 +9,16 @@
 
 int _isalpha(int c)
 {
-	int x;
-
-	for ((x = 'a'); x <= 'z'; x++) ||
-	    ((x = 'A'); x <= 'Z'; x++);
-
-	if (c == x)
+	if (c >= 'a' && c < 'z')
+	{
 		return (1);
+	}
+	else if (c >= 'A' && c < 'Z')
+	{
+		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
