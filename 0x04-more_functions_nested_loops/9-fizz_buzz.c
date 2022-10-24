ee4be86c8ace438dd2a592a68f7984a41a510b2b
olukayode olusegun <olukayodeoluseguno@gmail.com>
2022-10-24 02:58:41 -0700

    A program that prints the numbers from 1 to 100, followed by a new line.

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - program that prints either number
+ *
+ * Return: success
+ *
+ */
+
+int main(void)
+{
+	int x = x;
+
+	while (x <= 100)
+	{
+		if (x % 3 == 0 && x % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%x", x);
+		}
+		if (x != 100)
+		{
+			putchar(' ');
+		}
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
