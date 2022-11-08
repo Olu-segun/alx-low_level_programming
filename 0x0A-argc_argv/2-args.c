939b5eff4d6330091d0c6f7e0117e5130b860329
olukayode olusegun <olukayodeoluseguno@gmail.com>
2022-11-08 03:11:33 -0800

    A  program that prints all arguments it receives.

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#include "main.h"
+
+/**
+ * main - prints all arguments it received
+ * @argc: argc parameter
+ * @argv: an array of a command listed
+ * Return: 0 for success
+ */
+
+int main(int argc, char *argv[])
+
+{
+
+		int i;
+
+
+
+			for (i = 0; i < argc; i++)
+
+						printf("%s\n", argv[i]);
+
+
+
+				return (0);
+
+}
